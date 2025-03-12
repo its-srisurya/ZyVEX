e9c68ca2d44c3d81dbeebdb8bf0999a5fce3a207
its-srisurya <havanurusrisurya@gmai.com>
2025-03-12 08:29:19 +0530

    Second commit

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,8 +4,8 @@ const Navbar = () => {
     return (
         <nav className='bg-black text-white flex justify-between items-center h-15'>
             <div className="logo font-bold px-4 text-xl flex justify-center items-center gap-3">
-               <a href="/"><img src="/coffee.gif" width={24} alt="logo image" /></a> 
-               <a href="/"><span >ZyVEX</span></a> 
+               <a href="/"><img src="/logoo.jpg" width={27} alt="logo image" /></a> 
+               <a href="/" className='text-xxl'><span >ZyVEX</span></a> 
             </div>
             <ul className='flex justify-between items-center p-4 gap-3'>
                 <li>Home</li>
